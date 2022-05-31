816eefb15e1d7dea4b41f17ce56f688db65c42c4
shane gaignard <shane.c.gaignard@gmail.com>
2022-05-31 15:19:06 +0200

    added geocoder

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,14 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def index
+    produit = "produit=#{params[:query]}&"
+    latitude = params[:lattitude]
+    longititude = params[:longitude]
+    # vente_direct = "&filtrerVenteDetail=1"
+    # restaurant = "&filtrerRestaurants=1"
+
+    api_url = "https://opendata.agencebio.org/api/gouv/operateurs/?#{produit}lat=#{latitude}&lng=#{longitude}&nb=50&trierPar=%2F%2Fcoords&ordreTri=%2F%2Fdesc&filtrerVenteDetail=1"
+  end
 end
