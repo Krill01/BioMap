a811f900f07d1bc8b58600afcf2863c76310506b
Renaud BORLET <renaud_borlet@hotmail.com>
2022-05-31 14:54:27 +0200

    seed and active storage cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "faker"
+
+User.destroy_all
+
+file_user = URI.open('https://file1.closermag.fr/var/closermag/storage/images/media/images-des-contenus/galerie/2016-09-09-14-fois-ou-hugh-grant-nous-a-donne-envie-de-l-epouser-photos/hugh-grant12/5461072-1-fre-FR/Hugh-Grant.jpg?alias=original')
+user = User.new(
+  first_name: "Hugh",
+  last_name: "GRANT",
+  address: Faker::Address.full_address,
+  phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+  email: "hugh.grant@sfr.fr",
+  password: "biomap"
+  )
+user.photo.attach(io: file_user1, filename: "#{user.first_name}_#{user.last_name}", content_type: 'image/png')
+user.save!
