9458f290bebd252c974ad915ff0d94594c9fec4c
Renaud BORLET <renaud_borlet@hotmail.com>
2022-06-01 12:29:16 +0200

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,55 @@ class PagesController < ApplicationController
   end
 
   def index
-    produit = "produit=#{params[:query]}&"
-    latitude = params[:lattitude]
-    longititude = params[:longitude]
-    # vente_direct = "&filtrerVenteDetail=1"
-    # restaurant = "&filtrerRestaurants=1"
+    @produit = params[:search][:produit]
+    if @produit == ""
+      produit_search = ""
+    else
+      produit_search = "produit=#{@produit}&"
+    end
+    # # use geocoder to get address
+    address = params[:search][:address]
+    address_to_geo = Geocoder.search(address)
+    coordinates = address_to_geo.first.coordinates #ARRAY
+    @coordinates_hash = {
+      lat: coordinates[0],
+      lng: coordinates[1]
+    }
 
-    api_url = "https://opendata.agencebio.org/api/gouv/operateurs/?#{produit}lat=#{latitude}&lng=#{longitude}&nb=50&trierPar=%2F%2Fcoords&ordreTri=%2F%2Fdesc&filtrerVenteDetail=1"
+
+    # Renaud GEOCODED/MAPBOX
+    # @Producers = Producer.all
+    # # the `geocoded` scope filters only producers with coordinates (latitude & longitude)
+    # @markers = @producers.geocoded.map do |producer|
+    #   {
+    #     lat: producer.latitude,
+    #     lng: producer.longitude,
+    #     info_window: render_to_string(partial: "info_window", locals: { producer: producer })
+    #     image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
+    #   }
+
+    # Renaud GEOCODED/MAPBOX
+    # the `geocoded` scope filters only producers with coordinates (latitude & longitude)
+    # @location = @producers.geocoded.map do |producer|
+    #   {
+    #     lat: producer.latitude,
+    #     lng: producer.longitude,
+    #     # image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
+    #   }
+    # end
   end
 end
+
+  # def producteurs
+  #   map_data
+  #   render json: @data
+  # end
+
+  # def map_data
+  #   sleep(2)
+  #   @data = [
+  #     {
+  #       name:
+  #     }
+  #   ]
+  # end
