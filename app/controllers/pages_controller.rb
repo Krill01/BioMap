98f9561b489d5b982d6135bf219a37d0e4b9db69
Renaud BORLET <renaud_borlet@hotmail.com>
2022-06-01 16:33:09 +0200

    mapbox ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,27 +8,30 @@ class PagesController < ApplicationController
   end
 
   def index
-<<<<<<< HEAD
-    @produit = params[:search][:produit]
-    if @produit == ""
+    produit = params[:search][:produit]
+    if produit == ""
       produit_search = ""
     else
-      produit_search = "produit=#{@produit}&"
+      produit_search = "produit=#{produit}&"
     end
-    # # use geocoder to get address
+    # use geocoder to get address
     address = params[:search][:address]
     address_to_geo = Geocoder.search(address)
     coordinates = address_to_geo.first.coordinates #ARRAY
-    @coordinates_hash = {
+    @coordinates_hash = [{
       lat: coordinates[0],
       lng: coordinates[1]
-    }
+    }]
+    lattitude = coordinates[0]
+    longitude = coordinates[1]
 
+    @search_url = "https://opendata.agencebio.org/api/gouv/operateurs/?#{produit_search}lat=#{lattitude}&lng=#{longitude}&nb=50&trierPar=%2F%2Fcoords&ordreTri=%2F%2Fdesc&filtrerVenteDetail=1"
+  end
 
-    # Renaud GEOCODED/MAPBOX
+    # Renaud GEOCODED / MAPBOX
     # @Producers = Producer.all
-    # # the `geocoded` scope filters only producers with coordinates (latitude & longitude)
-    # @markers = @producers.geocoded.map do |producer|
+    # # the `geocoded` scope filters only producers with coordinates(latitude & longitude)
+    # @markers = @producers.geocoded.map do | producer |
     #   {
     #     lat: producer.latitude,
     #     lng: producer.longitude,
@@ -36,64 +39,18 @@ class PagesController < ApplicationController
     #     image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
     #   }
 
-    # Renaud GEOCODED/MAPBOX
-    # the `geocoded` scope filters only producers with coordinates (latitude & longitude)
-    # @location = @producers.geocoded.map do |producer|
-    #   {
-    #     lat: producer.latitude,
-    #     lng: producer.longitude,
-    #     # image_url: helpers.asset_url("REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS")
-    #   }
+
+    # def producteurs
+    #   map_data
+    #   render json: @data
     # end
-=======
-    # if params[:search][:produit] == ""
-    #   produit = ""
-    # else
-    #   produit = "produit=#{params[:search][:produit]}&"
+
+    # def map_data
+    #   sleep(2)
+    #   @data = [
+    #     {
+    #       name:
+    #     }
+    #   ]
     # end
-    # # # use geocoder to get address
-    # address = Geocoder.search(params[:search][:address])
-    # @coordinates = address.first.coordinates
-    # @produit =
-    # search_url = "https://opendata.agencebio.org/api/gouv/operateurs/?#{produit}lat=#{coordinates[0]}&lng=#{coordinates[1]}&nb=50&trierPar=%2F%2Fcoords&ordreTri=%2F%2Fdesc&filtrerVenteDetail=1"
-
-    # url_serialized = URI(search_url).read
-    # search_parsed = JSON.parse(url_serialized, symbolize_names: true)
-    # @results = seach.items
-
-
-    # latitude = params[:lattitude]
-    # longititude = params[:longitude]
-    # vente_direct = "&filtrerVenteDetail=1"
-    # restaurant = "&filtrerRestaurants=1"
-  end
-
-  def producteurs
-    map_data
-    render json: @data
-  end
-
-  def map_data
-    sleep(2)
-    @data = [
-      {
-        name:
-      }
-    ]
->>>>>>> master
-  end
 end
-
-  # def producteurs
-  #   map_data
-  #   render json: @data
-  # end
-
-  # def map_data
-  #   sleep(2)
-  #   @data = [
-  #     {
-  #       name:
-  #     }
-  #   ]
-  # end
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
   static values = {
     apiKey: String,
     location: Array,
-    // markers: Array
+    apiUrl: String,
   }
 
   connect() {
@@ -16,44 +16,68 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-    // this.#addMarkersToMap()
-    // this.#fitMapToMarkers()
+    this.#addLocationToMap()
+    this.#fitMapToLocation()
     this.map.addControl(new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl
     }))
+    this.searchProducer()
   }
-}
+
+  searchProducer() {
+    const url = this.apiUrlValue
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      this.addMarkersToMap(data);
+      this.fitMapToMarkers(data);
+    })
+  }
+
+  addMarkersToMap(data) {
+    data["items"].forEach((marker) => {
+      console.log(marker["adressesOperateurs"][0]["long"])
+      console.log(marker["adressesOperateurs"][0]["lat"])
+      // const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+      // Create a HTML element for your custom marker
+      // const customMarker = document.createElement("div")
+      // customMarker.className = "marker"
+      // customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      // customMarker.style.backgroundSize = "contain"
+      // customMarker.style.width = "25px"
+      // customMarker.style.height = "25px"
+      new mapboxgl.Marker()
+        .setLngLat([marker["adressesOperateurs"][0]["long"], marker["adressesOperateurs"][0]["lat"]])
+        // .setPopup(popup)
+        .addTo(this.map)
+    });
+  }
+
+  fitMapToMarkers(data) {
+    const bounds = new mapboxgl.LngLatBounds()
+    data["items"].forEach(marker => bounds.extend([marker["adressesOperateurs"][0]["long"], marker["adressesOperateurs"][0]["lat"]]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 100 })
+  }
 
 
-// private
+  #addLocationToMap() {
+    this.locationValue.forEach((location) => {
+      // const customLocation = document.createElement("div")
+      // customLocation.className = "location"
+      // customLocation.style.backgroundSize = "contain"
+      // customLocation.style.width = "25px"
+      // customLocation.style.height = "25px"
+      new mapboxgl.Marker()
+        .setLngLat([location.lng, location.lat])
+        .addTo(this.map)
+    });
+  }
 
-// #addMarkersToMap() {
-//   this.markersValue.forEach((marker) => {
-//     const popup = new mapboxgl.Popup().setHTML(marker.info_window)
-//     // Create a HTML element for your custom marker
-//     const customMarker = document.createElement("div")
-//     customMarker.className = "marker"
-//     customMarker.style.backgroundImage = `url('${marker.image_url}')`
-//     customMarker.style.backgroundSize = "contain"
-//     customMarker.style.width = "25px"
-//     customMarker.style.height = "25px"
-//     new mapboxgl.Marker(customMarker)
-//       .setLngLat([marker.lng, marker.lat])
-//       .setPopup(popup)
-//       .addTo(this.map)
-//   });
-// }
+  #fitMapToLocation() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.locationValue.forEach(location => bounds.extend([location.lng, location.lat]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
 
-// #fitMapToMarkers() {
-//   const bounds = new mapboxgl.LngLatBounds()
-//   this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
-//   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-// }
-
-
-#fitMapToLocation() {
-  const bounds = new mapboxgl.LngLatBounds()
-  bounds.extend([this.locationValue[1], this.locationValue[0]])
-  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
 }
