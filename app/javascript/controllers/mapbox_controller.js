b0201dfadb8faee8eaa0fce2fe7a070a1c9e2fed
Renaud BORLET <renaud_borlet@hotmail.com>
2022-06-03 16:54:54 +0200

    update map and marker

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
@@ -9,25 +10,37 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
+    this.markers = []
     this.map = new mapboxgl.Map({
       container: this.element,
       zoom: 10,
       center: this.locationValue,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-    this.addLocationToMap()
-    this.searchProducer()
+
+    this.map.addControl(this.geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }))
+
+    this.addLocationToMap(this.locationValue)
+    this.searchProducer(this.locationValue)
+
+    // this.geocoder.on('click', (e) => { this.map.setCenter{ this.center = e.features[0].geometry.coordinates } })
+    // console.log(this.center)
+    // this.markers.remove()
+    // this.addLocationToMap(this.center)
+    // this.searchProducer(this.center)
   }
 
-  addLocationToMap() {
+  addLocationToMap(data) {
     new mapboxgl.Marker({ "color": "#FD1015" })
-      .setLngLat([this.locationValue.lng, this.locationValue.lat])
+      .setLngLat([data.lng, data.lat])
       .addTo(this.map)
   }
 
-  searchProducer() {
-    const url = `/producers?lng=${this.locationValue.lng}&lat=${this.locationValue.lat}`
+  searchProducer(data) {
+    const url = `/producers?lng=${data.lng}&lat=${data.lat}`
     fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -40,10 +53,11 @@ export default class extends Controller {
       const popup = new mapboxgl.Popup()
                         .setHTML(producer.popup_html)
       // Create a HTML element for your custom marker
-      new mapboxgl.Marker()
+      markers.push(new mapboxgl.Marker()
         .setLngLat([producer["adressesOperateurs"][0]["long"], producer["adressesOperateurs"][0]["lat"]])
         .setPopup(popup)
         .addTo(this.map)
+      )
 
       // const favorite = () => {
       //   console.log('je veux mettre e favoris ce truc', productor)
@@ -55,5 +69,4 @@ export default class extends Controller {
       // });
     });
   }
-
 }
